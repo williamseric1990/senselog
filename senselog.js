27dc3f1439e1ea892df0ef0243286557c780153c
Cody Maden <signedlongint@gmail.com>
2015-07-17 12:05:45 -0400

    removed encapsulation - unnecessary

--- senselog.js
+++ senselog.js
@@ -3,42 +3,45 @@ var io = require('socket.io').Server(http);
 var MongoClient = require('mongodb').MongoClient;
 var config = require('./config/config.json');
 
-var server = function() {
-    io.on('connection', function(socket) {
+io.on('connection', function(socket) {
 
-        console.log('-> client connected');
+    console.log('-> client connected');
 
-        socket.on('feedback', function(value) {
-            if (value) {
-                console.log('<- pressed');
-                socket.emit('sensors');
-            } else {
-                console.log('<- released');
-            }
-        });
-
-        socket.on('sensor-data', function(data) {
-            console.log(data);
-        });
-
-        socket.on('node-data', function(data) {
-            console.log(data);
-        });
-
-        socket.on('all-data', function(data) {
-            console.log('<- recieved data from ' + socket.id);
-            MongoClient.connect(config.mongourl, function(err, db) {
-                var collection = db.collection(config.collection);
-                collection.insert(data);
-                db.close();
-            });
-        });
-
+    /* Prototype of feedback feature - customize at will */
+    socket.on('feedback', function(value) {
+        if (value) {
+            console.log('<- pressed');
+            socket.emit('sensors');
+        } else {
+            console.log('<- released');
+        }
     });
 
-    http.listen(3000, function() {
-        console.log('ready');
+    /* Data is discarded - placeholder for future features */
+    socket.on('sensor-data', function(data) {
+        console.log(data);
     });
-}
 
-module.export = server;
+    /* Data is discarded - placeholder for future features */
+    socket.on('node-data', function(data) {
+        console.log(data);
+    });
+
+    /* all-data signal
+     * Recieves combined node and sensor data, then inserts it into
+     * the configured MongoDB collection.
+     */
+    socket.on('all-data', function(data) {
+        console.log('<- recieved data from ' + socket.id);
+        MongoClient.connect(config.mongourl, function(err, db) {
+            var collection = db.collection(config.collection);
+            collection.insert(data);
+            db.close();
+        });
+    });
+
+});
+
+http.listen(3000, function() {
+    console.log('ready');
+});
