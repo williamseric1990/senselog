631842031a2fc13e5d57cf59b66b222c58c4e434
Cody Maden <randallmaden1497@flpoly.org>
2016-06-10 15:45:40 -0400

    data now unpacked properly

--- lib/SenselogClient.py
+++ lib/SenselogClient.py
@@ -2,11 +2,16 @@ import time
 import msgpack
 import socketserver
 
+
 DATA_ID = '\x01\x02'
 DATA_REQ = '\x01\x01'
 
+
 class SenseLogClient(socketserver.BaseRequestHandler):
     def handle(self):
-        data = self.request[0].strip()
+        r_data = self.request[0].strip()
         socket = self.request[1]
+
+        data = msgpack.unpackb(r_data)
+
         print('Sensor data recieved: {}'.format(data))
